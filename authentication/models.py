d0bfa6886d20147526b0a4ddc6151df30b6edb14
AntoniCabral <[cabral.antoni@hotmail.fr]>
2025-12-01 11:52:27 +0100

    Ajout du model itilisateur

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    CREATOR = 'CREATOR'
+    USER = 'USER'
+
+    ROLE_CHOICES = (
+        (CREATOR, 'Créateur'),
+        (USER, 'Utilisateur'),
+    )
+
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
